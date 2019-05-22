51dc433b468cab3fb522d2f4e83115c9803a0f8d
Sergio Ramirez <s1nicolasrg@gmail.com>
2019-05-22 13:36:41 -0500

    plantilla basica de nav y formularios

---
+++ models/enlaces.php
@@ -0,0 +1,18 @@
+<?php 
+
+class Paginas{
+	public function enlacesPaginasModel($enlaces){
+		if($enlaces == "ingresar" || $enlaces == "usuarios" || $enlaces == "editar" || $enlaces == "salir"){
+			$module =  "views/modules/".$enlaces.".php";
+		}
+		else if($enlaces == "index"){
+			$module =  "views/modules/registro.php";
+		}
+		else{
+			$module =  "views/modules/registro.php";
+		}
+		return $module;
+	}
+}
+
+?>
